c12231cb55d75cc19f78c42d89a1b3f48a85eefb
Robbie <robbiethewadley@gmail.com>
2025-07-09 12:24:21 -0600

    remove config

--- pm2.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-	name: "evil",
-	script: "index.ts",
-	interpreter: "bun",
-	env: {
-		PATH: `${process.env.HOME}/.bun/bin:${process.env.PATH}`, // Add "~/.bun/bin/bun" to PATH
-	},
-};
